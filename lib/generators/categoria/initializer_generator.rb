0c141721f6718195b813c203fd5e719024d2fcc3
Yaw Boakye <wheresyaw@gmail.com>
2024-06-06 14:04:55 +0100

    generator to create initializer
    in order to load all the domains under the application's namespace, we
    have to fudge around with the Rails autoloader a little bit. this piece
    of initializer code ensures that all domains are loaded under the
    application's namespace, and ready when you need them.

---
+++ lib/generators/categoria/initializer_generator.rb
@@ -0,0 +1,22 @@
+# typed: strict
+# frozen_string_literal: true
+
+module Categoria
+  module Generators
+    class InitializerGenerator < ::Rails::Generators::Base
+      sig { void }
+      def create_categoria_initializer_file
+        in_root do
+          create_file "config/initializers/categoria.rb", <<~INITIALIZER
+            # frozen_string_literal: true
+
+            Rails.autoloaders.main.push_dir(
+              "\#{Rails.root}/app/lib",
+              namespace: Rails.application.class.module_parent
+            )
+          INITIALIZER
+        end
+      end
+    end
+  end
+end
