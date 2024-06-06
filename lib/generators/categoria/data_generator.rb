93c7964f476fe68d3aef5ad6711c3579a5142495
Yaw Boakye <wheresyaw@gmail.com>
2024-06-06 15:03:40 +0100

    wip: data class generator

--- lib/generators/categoria/data_generator.rb
+++ lib/generators/categoria/data_generator.rb
@@ -7,6 +7,12 @@ module Categoria
       extend T::Sig
 
       source_root File.expand_path("templates", __dir__)
+
+      desc <<~DOC.squish
+        generates a data class for the domain. data classes are used
+        like value classes: hashes are more fit for the purpose except
+        that in the wild hashes are difficult to tame.
+      DOC
     end
   end
 end
